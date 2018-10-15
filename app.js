c5f9974e60345d86d7e13f0fef51893303340060
Lorenzo Borje <lorenzo.borje@gmail.com>
2018-10-15 18:18:43 +0800

    added multiple instruments

--- app.js
+++ app.js
@@ -1,52 +1,76 @@
 const beatMeasure = 16;
 const bpm = 110;
 const bpmPerMilli = 15000/bpm;
+const instruments = ['kick', 'snare', 'hihat', 'openhat'];
 let currentMeasure = 0;
+let currentlyPlaying;
 
+function playBeat(instrument) {
+    console.log(`hit ${instrument}`);
+    $(`audio[data-key='${instrument}']`)[0].currentTime = 0;
+    $(`audio[data-key='${instrument}']`)[0].play()
+}
 
 function playSequencer() {
     if (currentMeasure == 16) currentMeasure = 0;
-    if ($(`.${currentMeasure}`).hasClass('selected')) {
-        $('audio')[0].currentTime = 0;
-        $('audio')[0].play();
+    console.log(currentMeasure);
+    for (let i = 0; i < $(`.${currentMeasure}`).length; i++){
+        if ($(`.${currentMeasure}:eq(${i})`).hasClass('selected')) {
+            currentInstrument = $(`.${currentMeasure}:eq(${i})`).attr('data-key');
+            playBeat(currentInstrument);
+
+        }
     }
     currentMeasure++;
 }
 
+function switchPlayButton() {
+    $('.playbutton').toggleClass('hidden');
+    $('.stopbutton').toggleClass('hidden');
+}
+
 function togglePlay() {
-    setInterval(playSequencer, bpmPerMilli);
-
+    switchPlayButton();
+    if ($('.playbutton').hasClass('hidden')) {
+        currentlyPlaying = setInterval(playSequencer, bpmPerMilli);
+    } else {
+        clearInterval(currentlyPlaying);
+    }
 }
 
-function handlePlayButton() {
-    $('.playbutton').click(togglePlay());
+function handlePlayButton(){
+    $('.playbutton, .stopbutton').click(togglePlay);
 }
 
-function handleBeatSelection() {
+function handleBeatSelection() { 
     $('.pad').on('click', e => {
         $(event.currentTarget).toggleClass('selected');
-
+        let currentInstrument = $(event.currentTarget).attr('data-key');
         // resets currentTime so when other buttons are clicked it can start immediately playing
-        // repeatedly pressing buttons heightens pitch, odd side-effect
+        // chaining buttons heightens pitch, odd side-effect
         // better solutions exist (see: StackOverflow) but for first iteration this is fine
         if ($(event.currentTarget).hasClass('selected')) {
-            $('audio')[0].currentTime = 0;
-           $('audio')[0].play();
+            $(`audio[data-key="${currentInstrument}"]`)[0].currentTime = 0;
+           $(`audio[data-key="${currentInstrument}"]`)[0].play();
         }
     });
 }
 
-function generateSequencerGrid() {
+function generateDrumSequencerGrid() {
     let htmlString = '';
-    for (let i = 0; i < beatMeasure; i++) {
-        htmlString += `<button class="pad ${i+1}"></button>`;
+    for (let i = 0; i < instruments.length; i++) {
+         htmlString += `<div class="row instrument ${instruments[i]}">`;
+        for (let j = 0; j < beatMeasure; j++) {
+            htmlString += `<button class="pad ${j}" data-key="${instruments[i]}"></button>`;
+         }
+         htmlString += `</div>`
     }
     return htmlString;
 }
 
 function composeSequencer(){
     $('.sequencer').empty();
-    $('.sequencer').append(generateSequencerGrid);
+    $('.sequencer').append(generateDrumSequencerGrid);
 }
 
 function handleStart() {
